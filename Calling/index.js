b8c87a9f6f0bcd7d2e464436a69955ecadbcab4a
M0111Ange <angemukundente@gmail.com>
2023-12-01 11:32:01 +0200

    assignment updates

--- Calling/index.js
+++ Calling/index.js
@@ -6,18 +6,18 @@ const addedBook=
         title: 'The new book',
         author: 'Angela Smith',
         year: 1999 
-       }
+       };
+
+       const obj={
+        title:"This is the new book",
+        author: 'Uwera Ga'
+        }; 
 
 
 
        add(addedBook);
        display();
        update(1,'year',2000);
-
-       const obj={
-        title:"This is the new book",
-        author: 'Uwera Ga'
-        }  
       
        updateMany(1,obj);
        deleteBook(1);
